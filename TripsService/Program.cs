bdf7bdc90602067a74130242b331eabe83d2d405
Andres Salamanca <andrealebarbaritos@gmail.com>
2025-06-12 23:13:01 -0500

    Add skeleton for travel services

---
+++ TripsService/Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.OpenApi.Models;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "MINI API Clients", Version = "v1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter your JWT token in this format: Bearer <your-token>"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddMvc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapGet("/getDestionations", () =>
+{
+
+})
+.WithName("GetDestinations");
+
+app.MapPost("/createDestination", () =>
+{
+
+})
+.WithName("CreateDestinations");
+
+app.MapPut("/updateDestination", () =>
+{
+
+})
+.WithName("UpdateDestinations");
+
+app.MapDelete("/deleteDestination", () =>
+{
+
+})
+.WithName("DeleteDestinations");
+
+app.Run();
+
+
